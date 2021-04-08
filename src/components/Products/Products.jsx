5ffa4be45473c8d9d8989ef22bc956f10c103530
Peter Tim Soriano <chops10@my.yorku.ca>
2021-04-08 03:35:36 -0400

    Navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,9 +4,9 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 const products = [
-    { id: 1, name: 'Pheasant Tail', description: 'Nymph', price: '$3' },
-    { id: 2, name: 'Copper John', description: 'Nymph', price: '$3' },
-    { id: 3, name: 'Hair\'s Ear', description: 'Nymph', price: '$3' }
+    { id: 1, name: 'Pheasant Tail', description: 'Nymph', price: '$3', image: './Images/PTNymph.jpg' },
+    { id: 2, name: 'Copper John', description: 'Nymph', price: '$3', image: 'https://cdn.shopify.com/s/files/1/1342/4185/products/fullsizeoutput_804_2048x.jpeg?v=1490296635' },
+    { id: 3, name: 'Hair\'s Ear', description: 'Nymph', price: '$3', image: 'https://cdn.shopify.com/s/files/1/1342/4185/products/fullsizeoutput_804_2048x.jpeg?v=1490296635' }
 ];
 
 const Products = () => {
